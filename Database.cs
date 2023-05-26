a2a419732541f38129ce68210d915f8b20d87b8e
kradam <kradam@gmail.com>
2023-05-26 19:39:42 +0200

    first

---
+++ Database.cs
@@ -0,0 +1,175 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+
+public class DatabaseHelper
+{
+    private string connectionString;
+
+    public DatabaseHelper(string dbFilePath)
+    {
+        connectionString = $"Data Source={dbFilePath};Version=3;";
+    }
+
+    public List<string> RetrieveStructure()
+    {
+        List<string> structure = new List<string>();
+
+        using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+        {
+            connection.Open();
+
+            DataTable tablesSchema = connection.GetSchema("Tables");
+
+            foreach (DataRow tableRow in tablesSchema.Rows)
+            {
+                string tableName = (string)tableRow["TABLE_NAME"];
+                string tableStructure = RetrieveTableStructure(connection, tableName);
+                structure.Add(tableStructure);
+            }
+
+            connection.Close();
+        }
+
+        return structure;
+    }
+    
+
+
+
+    private string RetrieveTableStructure(SQLiteConnection connection, string tableName)
+    {
+        string structure = "";
+
+        using (SQLiteCommand command = new SQLiteCommand($"SELECT sql FROM sqlite_master WHERE type='table' AND name='{tableName}'", connection))
+        {
+            using (SQLiteDataReader reader = command.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    structure = reader.GetString(0);
+                }
+            }
+        }
+
+        return structure;
+    }
+
+
+
+    public DataSet ExecuteQuery(string query)
+    {
+        using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+        {
+            connection.Open();
+
+            using (SQLiteCommand command = new SQLiteCommand(query, connection))
+            {
+                using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(command))
+                {
+                    DataSet dataSet = new DataSet();
+                    adapter.Fill(dataSet);
+
+                    return dataSet;
+                }
+            }
+        }
+    }
+    public void DisplayQueryResultsOld(DataSet dataSet)
+    {
+        foreach (DataTable table in dataSet.Tables)
+        {
+            Console.WriteLine($"Table: {table.TableName}");
+            Console.WriteLine("---------------------------------------");
+
+            foreach (DataColumn column in table.Columns)
+            {
+                Console.Write($"{column.ColumnName}\t");
+            }
+
+            Console.WriteLine("\n---------------------------------------");
+
+            foreach (DataRow row in table.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write($"{item}\t");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+    }
+public void DisplayQueryResults(DataSet dataSet)
+{
+    int bufferWidth = Console.BufferWidth; // Get the console buffer width
+
+    foreach (DataTable table in dataSet.Tables)
+    {
+        // Find the maximum width for each column
+        int[] columnWidths = new int[table.Columns.Count];
+        foreach (DataColumn column in table.Columns)
+        {
+            int columnIndex = column.Ordinal;
+            int maxColumnWidth = column.ColumnName.Length;
+
+            foreach (DataRow row in table.Rows)
+            {
+                string value = row[columnIndex].ToString();
+                int cellWidth = value.Length;
+                if (cellWidth > maxColumnWidth)
+                    maxColumnWidth = cellWidth;
+            }
+
+            columnWidths[columnIndex] = maxColumnWidth;
+        }
+
+        // Display table header
+        Console.WriteLine($"Table: {table.TableName}");
+        Console.WriteLine("---------------------------------------");
+
+        // Display column names
+        for (int i = 0; i < table.Columns.Count; i++)
+        {
+            string columnName = table.Columns[i].ColumnName;
+            Console.Write($"{columnName.PadRight(columnWidths[i])}\t");
+        }
+
+        Console.WriteLine("\n---------------------------------------");
+
+        // Display rows
+        foreach (DataRow row in table.Rows)
+        {
+            for (int i = 0; i < table.Columns.Count; i++)
+            {
+                string value = row[i].ToString();
+                int columnWidth = Math.Min(columnWidths[i], bufferWidth - Console.CursorLeft);
+                Console.Write($"{value.PadRight(columnWidth)}\t");
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+    }
+}
+
+
+
+
+    
+
+    public void DisplayQueryResults(string query)
+    {
+        DataSet result = ExecuteQuery(query);
+        DisplayQueryResults(result);
+    }
+
+    //method returning table Artists
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using OpenAI.GPT3;
+using OpenAI.GPT3.Managers;
+using OpenAI.GPT3.ObjectModels;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+
+
+string dbFilePath = "C:\\Users\\adam.kruszewski\\sqlite\\chinook.db";
+DatabaseHelper databaseHelper = new DatabaseHelper(dbFilePath);
+List<string> tablesStructure = databaseHelper.RetrieveStructure();
+foreach (string tableStructure in tablesStructure)
+{
+    //  Console.WriteLine($"{j++}: {tableStructure}");
+}
+
+var openAiService = new OpenAIService(new OpenAiOptions()
+{
+    //read ApiKey from text file
+    ApiKey = File.ReadAllText("openai-api-key.txt")
+
+
+});
+
+// const string EndOfDatabaseStructure = "<<< END OF DATABASE STRUCTURE >>>";
+const string SQLOnlyRemark = "Answer only with SQL, dont send any other text.";
+
+List<ChatMessage> initialMessages = new()
+    {
+        ChatMessage.FromSystem("You are an SQL assistant. You convert the question to SQL and return only SQL."),
+
+    };
+
+foreach (string tableStructure in tablesStructure)
+{
+    initialMessages.Add(ChatMessage.FromUser(tableStructure));
+    
+}
+
+// initialMessages.Add(ChatMessage.FromUser(EndOfDatabaseStructure));
+
+while (true)
+{
+    Console.WriteLine("Enter a query in natural language (type '/q' or '/quit' to exit):");
+    string? input = Console.ReadLine();
+
+    if (input == "/q" || input == "/quit")
+    {
+        break; // Exit the loop
+    }
+    else if (string.IsNullOrEmpty(input))
+    {
+        continue; // Wait for input again
+    }
+    Console.WriteLine();
+    var messages = initialMessages.ToList();
+    messages.Add(ChatMessage.FromUser(input + ". " + SQLOnlyRemark));
+
+    Console.WriteLine();
+    
+    var completionResult = await openAiService.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest
+    {
+        Messages = messages,
+        Model = Models.ChatGpt3_5Turbo,
+        Temperature = 0.0f,
+    });
+
+    
+
+    if (completionResult.Error != null)
+    {
+        Console.WriteLine(completionResult.Error.Message);
+        continue;
+    }
+
+    if (completionResult.Successful)
+    {
+        Console.WriteLine($"Total tokens: {completionResult.Usage.TotalTokens}, prompt tokens {completionResult.Usage.PromptTokens} completion tokens: {completionResult.Usage.CompletionTokens}");
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine(completionResult.Choices.First().Message.Content);
+        Console.ResetColor();
+
+    }
+
+    Console.WriteLine();
+
+    if (completionResult.Choices.First().Message.Content.ToUpper().StartsWith("SELECT"))
+    {
+        databaseHelper.DisplayQueryResults(completionResult.Choices.First().Message.Content);
+    
+    }
+
+    
+
+
+}
+
+
+

c4cb4dc3fd04561ced375b530255677894e30344
kradam <kradam@gmail.com>
2023-10-13 18:29:49 +0200

    refactor main file

--- Program.cs
+++ Program.cs
@@ -1,45 +1,15 @@
-﻿using OpenAI.GPT3;
-using OpenAI.GPT3.Managers;
-using OpenAI.GPT3.ObjectModels;
-using OpenAI.GPT3.ObjectModels.RequestModels;
+﻿using OpenAI.GPT3.ObjectModels.RequestModels;
 
+OpenAi openAi = new ("openai-api-key.txt");
 string dbFilePath = "chinook.db";
-DatabaseHelper databaseHelper = new DatabaseHelper(dbFilePath);
-List<string> tablesStructure = databaseHelper.RetrieveStructure();
-foreach (string tableStructure in tablesStructure)
-{
-    //  Console.WriteLine($"{j++}: {tableStructure}");
-}
-
-var openAiService = new OpenAIService(new OpenAiOptions()
-{
-    //read ApiKey from text file
-    ApiKey = File.ReadAllText("openai-api-key.txt")
-
-
-});
-
-// const string EndOfDatabaseStructure = "<<< END OF DATABASE STRUCTURE >>>";
-const string SQLOnlyRemark = "Answer only with SQL, dont send any other text.";
-
-List<ChatMessage> initialMessages = new()
-    {
-        ChatMessage.FromSystem("You are an SQL assistant. You convert the question to SQL and return only SQL."),
-
-    };
-
-foreach (string tableStructure in tablesStructure)
-{
-    initialMessages.Add(ChatMessage.FromUser(tableStructure));
-    
-}
-
-// initialMessages.Add(ChatMessage.FromUser(EndOfDatabaseStructure));
+DatabaseHelper databaseHelper = new (dbFilePath);
+var chatMessages = ChatMessagesHelper.PrepareChatMessages(databaseHelper);
 
 while (true)
 {
     Console.WriteLine("Enter a query in natural language (type '/q' or '/quit' to exit):");
-    string? input = Console.ReadLine();
+
+    string? input = args.Length > 0 ? string.Join(" ", args) : Console.ReadLine();
 
     if (input == "/q" || input == "/quit")
     {
@@ -50,19 +20,12 @@ while (true)
         continue; // Wait for input again
     }
     Console.WriteLine();
-    var messages = initialMessages.ToList();
-    messages.Add(ChatMessage.FromUser(input + ". " + SQLOnlyRemark));
+
+    chatMessages.Add(ChatMessage.FromUser(input));
 
     Console.WriteLine();
-    
-    var completionResult = await openAiService.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest
-    {
-        Messages = messages,
-        Model = Models.ChatGpt3_5Turbo,
-        Temperature = 0.0f,
-    });
 
-    
+    var completionResult = await openAi.GetCompletionResults(chatMessages);
 
     if (completionResult.Error != null)
     {
@@ -74,10 +37,7 @@ while (true)
     {
         Console.WriteLine($"Total tokens: {completionResult.Usage.TotalTokens}, prompt tokens {completionResult.Usage.PromptTokens} completion tokens: {completionResult.Usage.CompletionTokens}");
         Console.WriteLine();
-        Console.ForegroundColor = ConsoleColor.Gray;
         Console.WriteLine(completionResult.Choices.First().Message.Content);
-        Console.ResetColor();
-
     }
 
     Console.WriteLine();
@@ -85,13 +45,10 @@ while (true)
     if (completionResult.Choices.First().Message.Content.ToUpper().StartsWith("SELECT"))
     {
         databaseHelper.DisplayQueryResults(completionResult.Choices.First().Message.Content);
-    
+
     }
 
-    
-
-
+    if (args.Length > 0)
+        break;
 }
-
-
 
